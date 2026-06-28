122726677671feda408625d88f3251285cddd8ae
LaVenganzaDelLadron <darkglitch5417@gmail.com>
2026-06-28 20:44:15 +0800

    upload a new version and remove the rewind.db

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="rewind",
-    version="0.1.0",
-    packages=find_packages(),
-    py_modules=["database", "rewind"],
-)
+if __name__ == "__main__":
+    setup()
+
